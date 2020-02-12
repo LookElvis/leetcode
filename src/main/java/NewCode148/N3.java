b7044e48250f373dd127fbcab3111b97cbea3a47
lucian <504866487@qq.com>
2020-02-12 11:06:35 +0800

    update

---
+++ src/main/java/NewCode148/N3.java
@@ -0,0 +1,25 @@
+package NewCode148;
+
+/**
+ * max-points-on-a-line
+ * Created by Elvis on 2020/1/16.
+ */
+public class N3 {
+    public static void main(String[] args) {
+
+    }
+
+    class Point {
+      int x;
+      int y;
+      Point() { x = 0; y = 0; }
+      Point(int a, int b) { x = a; y = b; }
+    }
+
+    public int maxPoints(Point[] points) {
+
+
+
+        return 0;
+    }
+}
