7903221d9936bdd36743d7941dbcd80a77bc4cad
lucian <504866487@qq.com>
2020-03-12 10:56:19 +0800

    update Day10

---
+++ src/main/java/Interview/Day10.java
@@ -0,0 +1,31 @@
+package Interview;
+
+/**
+ * https://leetcode-cn.com/problems/greatest-common-divisor-of-strings/
+ * Created by Elvis on 2020/3/12.
+ */
+public class Day10 {
+    public static void main(String[] args) {
+
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int num = getNum(str1.length(), str2.length());
+        return str1.substring(0, num);
+    }
+
+    // 求最大公约数
+    public int getNum(int num1, int num2) {
+        num1 = Math.abs(num1);
+        num2 = Math.abs(num2);
+        while (num2 != 0) {
+            int tmp = num1 % num2;
+            num1 = num2;
+            num2 = tmp;
+        }
+        return num1;
+    }
+}
