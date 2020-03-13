3214c07f8df8acf407a140a08dedc29416e30156
lucian <504866487@qq.com>
2020-03-13 11:38:29 +0800

    update Day11

---
+++ src/main/java/DailyInterview/Day11.java
@@ -0,0 +1,24 @@
+package DailyInterview;
+
+/**
+ * https://leetcode-cn.com/problems/majority-element/submissions/
+ * Created by Elvis on 2020/3/13.
+ */
+public class Day11 {
+    public int majorityElement(int[] nums) {
+        int most = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == most) {
+                count++;
+            } else {
+                count--;
+                if (count < 0) {
+                    most = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return most;
+    }
+}
