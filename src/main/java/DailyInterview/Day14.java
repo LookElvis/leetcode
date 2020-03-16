33c216c131e84dae807fbdec535771bad7189ec2
lucian <504866487@qq.com>
2020-03-16 10:41:14 +0800

    update

---
+++ src/main/java/DailyInterview/Day14.java
@@ -0,0 +1,28 @@
+package DailyInterview;
+
+/**
+ * Created by Elvis on 2020/3/16.
+ */
+public class Day14 {
+    public String compressString(String S) {
+        if (S.length() == 0 || S == "") {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        char t = S.charAt(0);
+        int count = 1;
+        for (int i = 1; i < S.length(); i++) {
+            if (S.charAt(i) == t) {
+                count++;
+            } else {
+                sb.append(t);
+                sb.append(count);
+                t = S.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(t);
+        sb.append(count);
+        return sb.length() < S.length() ? sb.toString() : S;
+    }
+}
