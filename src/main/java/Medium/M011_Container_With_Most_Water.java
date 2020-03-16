3812b08ab0023c4b2550c5f14311999daf593474
lucian <504866487@qq.com>
2020-03-16 11:31:00 +0800

    update

--- src/main/java/Medium/M011_Container_With_Most_Water.java
+++ src/main/java/Medium/M011_Container_With_Most_Water.java
@@ -7,9 +7,7 @@ package Medium;
 public class M011_Container_With_Most_Water {
     public int maxArea(int[] height) {
         int max = Integer.MIN_VALUE;
-        int tmp;
         for (int i = 1; i < height.length; i++) {
-            tmp = height[i];
             for (int j = 0; j < i; j++) {
                 if (height[j] <= height[i]) {
                     max = Math.max(max, height[j] * (i - j));
