4c28ce5eea5160c01343c8c968cd6aa312b6f4c4
lucian <504866487@qq.com>
2020-03-17 11:39:13 +0800

    update day15

---
+++ src/main/java/DailyInterview/Day15.java
@@ -0,0 +1,45 @@
+package DailyInterview;
+
+/**
+ * Created by Elvis on 2020/3/17.
+ */
+public class Day15 {
+    public int countCharacters(String[] words, String chars) {
+        int[] countChar = new int[26];
+        for (int i = 0; i < chars.length(); i++) {
+            countChar[chars.charAt(i) - 'a']++;
+        }
+
+        // for (int i = 0; i < countChar.length; i++) {
+        //     System.out.print(countChar[i] + " ");
+        // }
+        // System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            String t = words[i];
+            int j = 0;
+            boolean isMatch = true;
+            for (; j < t.length(); j++) {
+                if (--countChar[t.charAt(j) - 'a'] < 0) {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch) {
+                count += t.length();
+            }
+            for (int k = 0; k <= j && k < t.length(); k++) {
+                countChar[t.charAt(k) - 'a']++;
+            }
+
+            // for (int m = 0; m < countChar.length; m++) {
+            //     System.out.print(countChar[m] + " ");
+            // }
+            // System.out.println();
+        }
+
+        return count;
+    }
+
+}
