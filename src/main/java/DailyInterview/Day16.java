305f8c1ec424332e5e493ed6759e1f691f1066c2
lucian <504866487@qq.com>
2020-03-18 10:42:35 +0800

    update Day16

---
+++ src/main/java/DailyInterview/Day16.java
@@ -0,0 +1,12 @@
+package DailyInterview;
+
+/**
+ * https://leetcode-cn.com/problems/rectangle-overlap/submissions/
+ * Created by Elvis on 2020/3/18.
+ */
+public class Day16 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]) &&
+                Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
+    }
+}
