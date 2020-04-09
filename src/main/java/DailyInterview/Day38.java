c3010b4e1b0e1f53914bc78c242eb98b9ebcf7e8
lucian <504866487@qq.com>
2020-04-09 09:44:17 +0800

    update day38

---
+++ src/main/java/DailyInterview/Day38.java
@@ -0,0 +1,36 @@
+package DailyInterview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Elvis on 2020/4/9.
+ */
+public class Day38 {
+    List<String> list = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return list;
+        }
+        String res = "(";
+        int index = 1;
+        int leftCount = 1;
+        int rightCount = 0;
+        generate(res, index, leftCount, rightCount, n);
+        return list;
+    }
+
+    public void generate(String s, int index, int leftCount, int rightCount, int n) {
+        if (index == 2 * n - 1) {
+            list.add(s + ")");
+            return;
+        }
+        if (leftCount < n) {
+            generate(s + "(", index + 1, leftCount + 1, rightCount, n);
+        }
+
+        if (leftCount != rightCount) {
+            generate(s + ")", index + 1, leftCount, rightCount + 1, n);
+        }
+    }
+}
