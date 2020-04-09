92d9dca227e2ad93f80c10545c5abdf65cc9df2e
lucian <504866487@qq.com>
2020-04-09 11:53:39 +0800

    update E121

--- src/main/java/Easy/E101_200/E121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/main/java/Easy/E101_200/E121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -14,6 +14,18 @@ public class E121_Best_Time_to_Buy_and_Sell_Stock {
     }
 
     public static int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        // i-0代表未持有，i-1代表持有
+        int dp_i0 = 0;
+        int dp_i1 = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
+            dp_i1 = Math.max(dp_i1, -prices[i]);
+        }
+        return dp_i0;
+    }
+
+    public static int maxProfit1(int[] prices) {
         int max = -1;
         for (int i = 0; i < prices.length-1; i++) {
             for (int j = i+1; j < prices.length; j++) {
