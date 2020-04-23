140c7664f610a8d80bfeb27cb81ae890119d332b
lucian <504866487@qq.com>
2020-04-23 08:58:45 +0800

    update day49

---
+++ src/main/java/DailyInterview/Day21_40/Day49.java
@@ -0,0 +1,37 @@
+package DailyInterview.Day21_40;
+
+import PublicClass.TreeNode;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-right-side-view/
+ * Created by Elvis on 2020/4/22.
+ */
+public class Day49 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                root = queue.poll();
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                size--;
+            }
+            res.add(root.val);
+        }
+        return res;
+    }
+}
