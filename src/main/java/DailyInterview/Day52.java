2567bb7f986d09d7eb84b436deda99ddca5210a7
lucian <504866487@qq.com>
2020-04-25 20:35:58 +0800

    update

---
+++ src/main/java/DailyInterview/Day52.java
@@ -0,0 +1,38 @@
+package DailyInterview;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/permutations/
+ * Created by Elvis on 2020/4/25.
+ */
+public class Day52 {
+    public static void main(String[] args) {
+
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+    int n = 0;
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        n = nums.length;
+        generate(list, 0);
+        return res;
+    }
+
+    public void generate(List<Integer> list, int first) {
+        if (first == n) {
+            res.add(new ArrayList<>(list));
+        }
+        for (int i = first; i < n; i++) {
+            Collections.swap(list, first, i);
+            generate(list, first + 1);
+            Collections.swap(list, first, i);
+        }
+    }
+}
