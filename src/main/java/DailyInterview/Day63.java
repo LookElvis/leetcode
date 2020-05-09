3216ba01ae1913a2e9b12858a264e00e4b3a1798
liuxiang <liuxiang38@meituan.com>
2020-05-09 17:25:44 +0800

    update day63

---
+++ src/main/java/DailyInterview/Day63.java
@@ -0,0 +1,26 @@
+package DailyInterview;
+
+/**
+ * https://leetcode-cn.com/problems/sqrtx/
+ * Created by Elvis on 2020/5/9.
+ */
+public class Day63 {
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(new Day63().mySqrt(4));
+    }
+
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
